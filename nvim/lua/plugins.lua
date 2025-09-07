c35e2b1f18642ef48cf4a20d3ee2e9d24dc647ed
bryam <nunezb.uni-greifswald.de>
2025-09-07 16:31:02 +0200

    proper plugging set up

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,12 +3,12 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
--- Beginnig of Plugins
+-- Beginning of Plugins
 
 Plug('akinsho/toggleterm.nvim',{ ['tag'] = '*' })
 Plug('ggandor/leap.nvim')
-Plug("nvim-treesitter/nvim-treesitter", {branch = 'master', lazy = false, build = ":TSUpdate"})
-Plug('catppuccin/nvim', {as = 'cappuccin'})
+Plug("nvim-treesitter/nvim-treesitter", { ['do'] = ":TSUpdate" })
+Plug('catppuccin/nvim', { ['as'] = 'cappuccin' })
 
 -- End of Plugins
 vim.call('plug#end')
