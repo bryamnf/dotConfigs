fdeb240f0b3f7e71b7c075ae4eb5e181c749ec0e
bryam <nunezb.uni-greifswald.de>
2025-09-07 13:46:15 +0200

    working color scheme, but not fully done yet

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,4 +1,4 @@
 require('config.leap')
 require('config.toggleterm')
 require('config.treesitter')
-
+require('config.colorscheme')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,6 +8,7 @@ vim.call('plug#begin')
 Plug('akinsho/toggleterm.nvim',{ ['tag'] = '*' })
 Plug('ggandor/leap.nvim')
 Plug("nvim-treesitter/nvim-treesitter", {branch = 'master', lazy = false, build = ":TSUpdate"})
+Plug('catppuccin/nvim', {as = 'cappuccin'})
 
 -- End of Plugins
 vim.call('plug#end')
