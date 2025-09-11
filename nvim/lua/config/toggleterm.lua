91b5d82e94ce9712b3ce6bcfe457111316c8e4a3
bryam <nunezb@uni-greifswald.de>
2025-09-11 07:41:07 +0200

    basic toggleterm without lag

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -1,19 +1,4 @@
-require("toggleterm").setup()
-----------------------------------------------------------------
-
----------Creating toggle for floating terminal with only one keymap---------
--- create a dedicated floating terminal
-local Terminal = require("toggleterm.terminal").Terminal
-local float_term = Terminal:new({
-  direction = "float",
-  close_on_exit = true,   -- auto-close when process exits
-})
-
--- toggle function
-function _FLOAT_TERM_TOGGLE()
-  float_term:toggle()
-end
-
--- keymap to toggle it
---vim.keymap.set({"n", "t"}, "<leader>t", _FLOAT_TERM_TOGGLE, { noremap = true, silent = true })
-------------------------------------------------------------------------------
+require("toggleterm").setup{
+	close_on_exit = true,
+ 	open_mapping = [[<c-\>]],
+}
