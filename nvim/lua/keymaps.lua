75271b63ee83cd758540f90be460fec360f55d28
bryam <nunezb@uni-greifswald.de>
2025-09-13 23:49:58 +0200

    simplyfing

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,14 +2,10 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Cursor movements
-keymap("n", "<leader>l", "$", opts)           -- End of line
-keymap("n", "<leader>h", "^", opts)           -- Beginning of line
-keymap("n", "<leader>k", "gg", opts)          -- Top of file
-keymap("n", "<leader>j", "G", opts)           -- Bottom of file
-keymap("v", "<leader>l", "$", opts)           -- End of line
-keymap("v", "<leader>h", "^", opts)           -- Beginning of line
-keymap("v", "<leader>k", "gg", opts)          -- Top of file
-keymap("v", "<leader>j", "G", opts)           -- Bottom of file
+keymap({'n','v'}, "<leader>l", "$", opts)           -- End of line
+keymap({'n','v'}, "<leader>h", "^", opts)           -- Beginning of line
+keymap({'n','v'}, "<leader>k", "gg", opts)          -- Top of file
+keymap({'n','v'}, "<leader>j", "G", opts)           -- Bottom of file
 
 -- Windows
 keymap({'n','v','t'}, '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
