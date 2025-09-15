ccbb4e3ab6a3b40cfdeaae0ae64343a6422431c0
bryam <nunezb@uni-greifswald.de>
2025-09-15 13:27:26 +0200

    added installation of neovim by snap

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@
 set -e #fail at error!
 
 # Set up nvim
+sudo snap install nvim --classic
+sudo apt update -y 
+sudo apt install tree-sitter-cli -y
+
 ln -sfn ~/dotconfigs/nvim ~/.config/nvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-sudo apt install tree-sitter-cli
