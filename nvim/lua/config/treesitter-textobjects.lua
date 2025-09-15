bc5f6109ca77ddd0ff2a8dfa0d5faf229f6442ad
bryam <nunezb@uni-greifswald.de>
2025-09-15 13:13:53 +0200

    forgot textobjects.lua

---
+++ nvim/lua/config/treesitter-textobjects.lua
@@ -0,0 +1,97 @@
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+
+    select = {
+      enable = true,
+      lookahead = false, -- like targets.vim
+      keymaps = {
+	-- Functions
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+
+        -- Classes
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+
+        -- Blocks (e.g. do...end, { ... })
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+
+        -- Conditionals (if, unless, etc.)
+        ["a?"] = "@conditional.outer",
+        ["i?"] = "@conditional.inner",
+
+        -- Loops
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+
+        -- Parameters
+        ["ap"] = "@parameter.outer",
+        ["ip"] = "@parameter.inner",
+
+        -- Assignments
+        ["a="] = "@assignment.outer",
+        ["i="] = "@assignment.inner",
+        ["l="] = "@assignment.lhs",
+        ["r="] = "@assignment.rhs",
+
+        -- Calls
+        ["afc"] = "@call.outer",
+        ["ifc"] = "@call.inner",
+
+        -- Returns
+        ["ar"] = "@return.outer",
+        ["ir"] = "@return.inner",
+
+        -- Comments
+        ["a/"] = "@comment.outer",
+        ["i/"] = "@comment.inner",
+
+        -- Numbers (useful in math-heavy code)
+        ["an"] = "@number.inner",
+
+        -- Attributes (e.g. decorators, annotations)
+        ["aa"] = "@attribute.outer",
+        ["ia"] = "@attribute.inner",
+
+        -- Regex
+        ["a/"] = "@regex.outer",
+        ["i/"] = "@regex.inner",
+
+        -- Statements
+        ["as"] = "@statement.outer",
+
+        -- Frame (in languages that support it)
+        ["afm"] = "@frame.outer",
+        ["ifm"] = "@frame.inner",
+
+        -- Scopename (like variable or function name in declaration)
+        ["is"] = "@scopename.inner",
+	},
+    },
+
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+    },
+
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+
+  },
+}
