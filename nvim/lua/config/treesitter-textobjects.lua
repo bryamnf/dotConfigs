1749c5704a5b62ee1cac06a72b1c2ddf3f1c5b2b
bryam <nunezb@uni-greifswald.de>
2025-09-15 23:20:23 +0200

    added keymaps to ts-text objects

--- nvim/lua/config/treesitter-textobjects.lua
+++ nvim/lua/config/treesitter-textobjects.lua
@@ -126,3 +126,15 @@ require'nvim-treesitter.configs'.setup {
     },
   },
 }
+-- Repeatable movements with ; and ,
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+-- Always forward with ; and backward with ,
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, "<", ts_repeat_move.repeat_last_move_previous)
+
+-- Optionally: make f/F/t/T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
