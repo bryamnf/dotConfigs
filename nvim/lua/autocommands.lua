6622ccdb73f1421729ab2acd77e93dea547d0109
bryam <nunezb@uni-greifswald.de>
2025-09-19 23:02:56 +0200

    normal keymaps for netrw

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,29 +1,28 @@
-local opts = { noremap = true, silent = true }
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
 -- relative numbers in netrw
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "netrw",
-  callback = function()
-    vim.opt_local.relativenumber = true
-  end,
+	pattern = "netrw",
+	callback = function()
+		vim.opt_local.relativenumber = true
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "netrw",
-  callback = function()
-    local bufopts = { noremap = true, silent = true, buffer = true }
-    keymap({'n','v'}, "<leader>l", "$", opts)
-    keymap({'n','v'}, "<leader>h", "^", opts)
-    keymap({'n','v'}, "<leader>k", "gg", opts)
-    keymap({'n','v'}, "<leader>j", "G", opts)
-    keymap('n', '<C-h>', [[<Cmd>wincmd h<CR>]], bufopts)
-    keymap('n', '<C-j>', [[<Cmd>wincmd j<CR>]], bufopts)
-    keymap('n', '<C-k>', [[<Cmd>wincmd k<CR>]], bufopts)
-    keymap('n', '<C-l>', [[<Cmd>wincmd l<CR>]], bufopts)
-    keymap('n', '<C-\\>', [[<Cmd>wincmd s<CR>]], bufopts)
-    keymap('n', '<C-s>', [[<Cmd>wincmd v<CR>]], bufopts)
-    keymap('n', '<C-o>', [[<Cmd>wincmd o<CR>]], bufopts)
-    keymap('n', '<C-q>', [[<Cmd>wincmd q<CR>]], bufopts)
-  end,
+	pattern = "netrw",
+	callback = function()
+		local opts = { noremap = true, silent = true, buffer = true }
+		map({ 'n', 'v' }, "<leader>l", "$", opts)
+		map({ 'n', 'v' }, "<leader>h", "^", opts)
+		map({ 'n', 'v' }, "<leader>k", "gg", opts)
+		map({ 'n', 'v' }, "<leader>j", "G", opts)
+		map('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+		map('n', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+		map('n', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+		map('n', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+		map('n', '<C-\\>', [[<Cmd>wincmd s<CR>]], opts)
+		map('n', '<C-s>', [[<Cmd>wincmd v<CR>]], opts)
+		map('n', '<C-o>', [[<Cmd>wincmd o<CR>]], opts)
+		map('n', '<C-q>', [[<Cmd>wincmd q<CR>]], opts)
+	end,
 })
