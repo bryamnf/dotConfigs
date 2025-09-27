acb12e47cf27d51d2f0a229402712f93bd106d36
bryam <nunezb@uni-greifswald.de>
2025-09-27 23:10:58 +0200

    using keymap instead of map

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,4 +1,4 @@
-local map = vim.keymap.set
+local keymap = vim.keymap.set
 
 -- relative numbers in netrw
 vim.api.nvim_create_autocmd("FileType", {
@@ -12,17 +12,17 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "netrw",
 	callback = function()
 		local opts = { noremap = true, silent = true, buffer = true }
-		map({ 'n', 'v' }, "<leader>l", "$", opts)
-		map({ 'n', 'v' }, "<leader>h", "^", opts)
-		map({ 'n', 'v' }, "<leader>k", "gg", opts)
-		map({ 'n', 'v' }, "<leader>j", "G", opts)
-		map('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-		map('n', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-		map('n', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-		map('n', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-		map('n', '<C-\\>', [[<Cmd>wincmd s<CR>]], opts)
-		map('n', '<C-s>', [[<Cmd>wincmd v<CR>]], opts)
-		map('n', '<C-o>', [[<Cmd>wincmd o<CR>]], opts)
-		map('n', '<C-q>', [[<Cmd>wincmd q<CR>]], opts)
+		keymap({ 'n', 'v' }, "<leader>l", "$", opts)
+		keymap({ 'n', 'v' }, "<leader>h", "^", opts)
+		keymap({ 'n', 'v' }, "<leader>k", "gg", opts)
+		keymap({ 'n', 'v' }, "<leader>j", "G", opts)
+		keymap('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+		keymap('n', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+		keymap('n', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+		keymap('n', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+		keymap('n', '<C-\\>', [[<Cmd>wincmd s<CR>]], opts)
+		keymap('n', '<C-s>', [[<Cmd>wincmd v<CR>]], opts)
+		keymap('n', '<C-o>', [[<Cmd>wincmd o<CR>]], opts)
+		keymap('n', '<C-q>', [[<Cmd>wincmd q<CR>]], opts)
 	end,
 })
