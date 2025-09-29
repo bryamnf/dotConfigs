9d4e2af7da38e35bfdb758f3b149abe25275740b
bryam <nunezb@uni-greifswald.de>
2025-09-29 12:34:38 +0200

    not needed

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,9 +10,6 @@ vim.api.nvim_set_option("clipboard", "unnamedplus")
 -- Add parser directory to runtimepath
 vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/parsers")
 
--- Add spell directory for spellcheck files
-vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/spell")
-
 -- Enable built-in lua module caching
 vim.loader.enable()
 
