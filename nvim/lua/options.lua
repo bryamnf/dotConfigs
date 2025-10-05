1fd7068faeb14146c94881769f45ea468289187e
bryam <nunezb@uni-greifswald.de>
2025-10-05 15:07:49 +0200

    tab equal two spaces

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -13,5 +13,11 @@ vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/parsers")
 -- Enable built-in lua module caching
 vim.loader.enable()
 
--- Number of spaces tabs count for
-vim.opt.tabstop = 2
+
+-- Set tab and indentation to 2 spaces
+vim.opt.tabstop = 2        -- Number of visual spaces per TAB
+vim.opt.softtabstop = 2    -- Number of spaces in tab when editing
+vim.opt.shiftwidth = 2     -- Number of spaces for each indentation
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Autoindent new lines
+vim.opt.autoindent = true  -- Copy indent from current line when starting a new one
