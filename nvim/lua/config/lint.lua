c2711e29b093987631c1ba77bedb2d9d38d092d6
bryam <nunezb@uni-greifswald.de>
2025-10-20 23:25:13 +0200

    it seems the linting is might work

--- nvim/lua/config/lint.lua
+++ nvim/lua/config/lint.lua
@@ -1,9 +1,11 @@
-require('lint').linters_by_ft = {
+local lint = require('lint')
+
+lint.linters_by_ft = {
   python = { 'ruff' },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
   callback = function()
-    require("lint").try_lint()
+    lint.try_lint()
   end,
 })
