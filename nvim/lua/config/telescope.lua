2377ee68b7d19777ccf9f3ec26706b091adc42f5
bryam <nunezb@uni-greifswald.de>
2025-10-21 22:46:29 +0200

    following primagen in telescope

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>tg', builtin.git_files, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>tw', function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
